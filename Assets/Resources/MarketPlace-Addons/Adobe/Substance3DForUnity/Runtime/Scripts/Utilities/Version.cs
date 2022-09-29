29d1b45dc6b72461f34109ab55ca2e60b69a2556
xreasler <j.o.n.t.e@hotmail.com>
2022-09-29 16:30:35 +0200

    Update
    k

--- Assets/Resources/MarketPlace-Addons/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
+++
@@ -1,9 +0,0 @@
-namespace Adobe.Substance
-{
-    public static class Version
-    {
-        public const string PluginVersion = "3.2.0-7-g390d6cb";
-        public const string EngineVersion = "8.2.0 0138644d";
-        public const int ImporterVersion = 320;
-    }
-}
