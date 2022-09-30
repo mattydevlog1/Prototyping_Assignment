c84d0a4093d1218b10b9361ddeceef2bff50d70b
Matty.devlog <99949556+mattydevlog@users.noreply.github.com>
2022-09-30 12:15:46 +0200

    Mouselook_and_playerMove

--- Assets/Resources/Scripts/CameraMovement.cs
+++ Assets/Resources/Scripts/CameraMovement.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements.Experimental;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
-    // Update is called once per frame
+
     void Update()
     {
         
+
+
     }
 }
--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,10 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private GameObject gun;
+
+    RaycastHit hit;
+    LayerMask enemy;
+    Ray ray;
+
+    private float shootRange = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     public void Fire(InputAction.CallbackContext context)
     {
         Shoot();
@@ -12,10 +34,22 @@ public class Player : MonoBehaviour
     void Shoot()
     {
         Debug.Log("You shot!");
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * shootRange, Color.yellow);
+
+
+
+
+        if (Physics.Raycast(gun.transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, enemy))
+        {
+            //  Debug.DrawRay(gun.transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
     }
 
     private void Update()
     {
+
         if (Input.mousePosition.y > 0)
         {
 
@@ -28,7 +62,37 @@ public class Player : MonoBehaviour
             mousePos.y = mousePos.y - objectPos.y;
 
             float angle = Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, angle - 90, 0));
+            transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
         }
+
+
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -transform.right * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = transform.forward * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -transform.forward * moveSpeed;
+        }
+
+    }
+
 }
+
+
+
